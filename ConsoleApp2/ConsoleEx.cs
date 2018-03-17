bac6abb40eb394cf5fd50e0a8f36d90ece81fb23
Rafał Derlatka <r.derlatka@gmail.com>
2018-03-17 16:56:36 +0100

    Initial commit

---
+++ ConsoleApp2/ConsoleEx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public static class ConsoleEx
+    {
+		public static void Write(ConsoleColor color, string line, params object[] args)
+		{
+			var currentColor = Console.ForegroundColor;
+
+			Console.ForegroundColor = color;
+			Console.Write(line, args);
+
+			Console.ForegroundColor = currentColor;
+		}
+
+		public static void WriteLine(ConsoleColor color, string line, params object[] args)
+		{
+			var currentColor = Console.ForegroundColor;
+
+			Console.ForegroundColor = color;
+			Console.WriteLine(line, args);
+
+			Console.ForegroundColor = currentColor;
+		}
+    }
+}
---
+++ ConsoleApp2/TaskModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class TaskModel
+    {
+		public string Description { get; set; }
+
+		public DateTime StartDate { get; set; }
+
+		public DateTime? EndDate { get; set; }
+
+		public bool IsImportant { get; set; }
+
+		public bool IsAllDayTask { get; set; }
+
+		public TaskModel(string description, DateTime from, DateTime? to, bool isImportant)
+		{
+			Description = description;
+			StartDate = from;
+			EndDate = to;
+			IsImportant = isImportant;
+
+			if (to.HasValue)
+			{
+				IsAllDayTask = true;
+			}
+		}
+	}
+}
